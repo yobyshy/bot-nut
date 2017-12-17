b20179373ca22adac32ad17a6fe8c6aeabd9d712
yobyshy <32682655+yobyshy@users.noreply.github.com>
2017-12-17 16:27:51 +0800

    Update app.js

--- app.js
+++ app.js
@@ -55,25 +55,26 @@ if (command === "mixed") {
       let novRole = msg.guild.roles.find("name", "Novice PUGs");
       let novnotify = msg.guild.roles.find("name", "NovNotify");
       let member = msg.member;
+      if(msg.member.roles.has(bgnRole.id)) {
+        member.removeRole(bgnRole);
+        msg.reply("You will no longer be notified for PUGs");
+            }
+      if(!msg.member.roles.has(bgnRole.id)) {
+        member.addRole(bgnRole);
+        msg.reply("You will now be notified for PUGs");
+      }
       if(msg.member.roles.has(novRole.id)) {
             if(!msg.member.roles.has(novnotify.id))
-            if(!msg.member.roles.has(bgnRole.id))
             member.addRole(novnotify);
-            member.addRole(bgnRole);
-            msg.reply("You will now be notified for PUGs");
-            }
+      }
           if(msg.member.roles.has(novnotify.id)) {
             member.removeRole(novnotify);
-            member.removeRole(bgnRole);
-            msg.reply("You will no longer be notified for PUGs");
           }
           if(msg.member.roles.has(modRole.id)) {
-          member.removeRole(bgnRole);
           member.removeRole(advRole);
            }
           if(msg.member.roles.has(modRole.id)) {
               if(!msg.member.roles.has(advRole.id))
-              member.addRole(bgnRole);
               member.addRole(advRole);
                }
       
